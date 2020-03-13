d12aeabfbbafc62c586140d68b4ef8a728763f0f
Miki Niwa <niwa@colla-tech.co.jp>
2020-03-13 21:46:24 +0900

    ・メタタグ修正 　・URL,SNS共有イメージ ・Twitterリンク追加 ・TOPページ開催時間、開催場所の文字サイズと表示修正 ・コンタクトフォーム（google form）設置 ・about,artist,timetableページの表示を「準備中」に修正 ・slickを削除

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,4 @@ $(window).on('load',function(){
     var loc = location.href;
     var file = loc.substring(loc.lastIndexOf("/")+1,loc.length);
     $('a[href$="'+file+'"]').addClass('active');
-
-    // slider
-    $('.fade').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
-    alert($("h1").text());
 });
