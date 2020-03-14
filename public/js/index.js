084385af930dedb08a45a17cdd1dc327ff5ce845
Miki Niwa <niwa@colla-tech.co.jp>
2020-03-14 19:03:36 +0900

    ・各ページ表示内容修正 ・画像追加変更 ・TOPページの画像表示にアニメーション追加 ・TOPスクロールボタン追加

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,25 @@
 $(window).on('load',function(){
+    // 閲覧中のページメニューに下線をつける
     var loc = location.href;
     var file = loc.substring(loc.lastIndexOf("/")+1,loc.length);
     $('a[href$="'+file+'"]').addClass('active');
+
+    // TOP画像表示アニメーション
+    $('.baseImage').hide().fadeIn(2000);
+    $('.coverImage').hide().fadeIn(3800);
+    
+    // 上から100pxスクロールした場合、TOPスクロールボタンを表示
+    var scrollTop = $('.fa-chevron-circle-up');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 110) {
+            scrollTop.fadeIn(2500);
+        } else {
+            scrollTop.fadeOut();
+        }
+    });
+    // スクロールボタン押下時アニメーション
+    scrollTop.click(function () {
+        $('body, html').animate({scrollTop: 0}, 500);
+        return false;
+    });
 });
